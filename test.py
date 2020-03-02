64b4e28daea6688bffd18acb087c2e9aa32c0cee
shine2shine <shine.automatic@gmail.com>
2020-03-02 13:34:31 +0800

    copy test.py to git

---
+++ test.py
@@ -0,0 +1,25 @@
+import random
+import time
+import pyautogui
+import cv2
+
+im1 = pyautogui.screenshot(region=(400,0,300,70))
+time.sleep(60)
+im2 = pyautogui.screenshot(region=(400,0,300,70))
+im1.show()
+im2.show()
+exit()
+def calc(start,end,initValue):
+    t = 0
+    
+    for i in range(end-start):
+        t = t + initValue * 1.15 ** i
+    return t
+
+
+
+
+print(calc(170,200,464.24))
+print(calc(50, 75, 6.01))
+
+
